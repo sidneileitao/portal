2a88fd716be109325ec97fd86c74fe611c872657
sidneileitao <sidneileitao@gmail.com>
2020-06-20 19:02:40 -0300

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -24,9 +24,9 @@ function retornaFornecedores(categoria)
 {
   var aFornecedores = [];
   
-  aFornecedores.push(["Padaria Santa Maria",["descrição","+5511999473653","+5511999473653","1136217788","dsdasa@gmail.com","www.google.com.br","/imagens/padaria.jpg",]]) ;
-  aFornecedores.push(["Pães & Doces Da Villa",["descrição","+5511999473653","+5511999473653","1136217788","dsdasa@gmail.com","www.google.com.br","/imagens/padaria.jpg",]]) ;
-  aFornecedores.push(["Remédios Pães Artesanais",["descrição","+5511999473653","+5511999473653","1136217788","dsdasa@gmail.com","www.google.com.br","/imagens/padaria.jpg",]]) ;
+  aFornecedores.push(["Padaria Santa Maria",["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean luctus faucibus nisl, tincidunt cursus massa gravida vel. Aenean feugiat tortor vitae nibh facilisis, a lobortis lacus tincidunt.","+5511999473653","+5511999473653","1136217788","dsdasa@gmail.com","www.google.com.br","/imagens/padaria.jpg",]]) ;
+  aFornecedores.push(["Pães & Doces Da Villa",["Cras at tellus enim. Sed a leo eget nulla hendrerit molestie sit amet eget elit. Integer at dictum tortor. Cras aliquet posuere augue, lacinia commodo urna varius vitae.","+5511999473653","+5511999473653","1136217788","dsdasa@gmail.com","www.google.com.br","/imagens/padaria.jpg",]]) ;
+  aFornecedores.push(["Remédios Pães Artesanais",["Integer eu diam in diam gravida dictum. Praesent nec magna facilisis, interdum ipsum eu, mattis diam. Nam sed metus tortor. Vivamus commodo urna nec consequat commodo.","+5511999473653","+5511999473653","1136217788","dsdasa@gmail.com","www.google.com.br","/imagens/padaria.jpg",]]) ;
   
   return aFornecedores;
 
